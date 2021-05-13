b1f96d2186bd4cf0ac7e538d5f6f973ecd8a239d
enyangxxx <ineffab@gmail.com>
2021-05-13 19:54:40 +0200

    small adjustments

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,41 +1,40 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React from 'react';
-import './Tab.css';
+import React from "react";
 import * as microsoftTeams from "@microsoft/teams-js";
-import mockup from '../assets/VEOMockup.jpg';
+import mockup from "../assets/VEOMockup.jpg";
 
 /**
  * The 'PersonalTab' component renders the main tab content
  * of your app.
  */
 class Tab extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      context: {}
-    }
+      context: {},
+    };
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     // Get the user context from Teams and set it in the state
     microsoftTeams.getContext((context, error) => {
       this.setState({
-        context: context
+        context: context,
       });
     });
     // Next steps: Error handling using the error object
   }
 
   render() {
-      return (
+    return (
       <div>
         <img src={mockup} alt="Oh no, something is wrong."></img>
       </div>
-      );
+    );
   }
 }
 export default Tab;
